cd12c79a232e7a77e90421e0d72179f43a23e16c
ANU YADAV <163430575+yadavanu63@users.noreply.github.com>
2024-05-01 01:22:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = API_ID = 24478182
 
 API_HASH = os.environ.get("API_HASH", "a98b5be0127986be1cc2553dbd99765e")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7109707094:AAGwjPkyO78A-7gX3vcQnA5T6zRTXUc1-Ik")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7139260466:AAEHwNQrZnGb8fzuQhNxEvlAl6Ud5_T_ddU")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
